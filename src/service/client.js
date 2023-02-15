961a587d1f6e32452d8e7ce999072607c1b35708
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2023-02-15 10:17:28 -0300

    finalizado comunicação do client com back no front

---
+++ src/service/client.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const client = axios.create({
+    baseURL: 'http://localhost:9000'
+})
+
+client.get('/movies')
+.then(res => {
+    console.log(res.data)
+})
+.catch(error => {
+    console.log(error)
+})
